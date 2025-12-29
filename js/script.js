64ee7600639d95bccd57ac7ad77be4f242e8f577
yenaaaaaaaaaaaaaaa <yeana7777@gmail.com>
2025-12-29 22:16:05 +0900

    메인화면 헤더, 메뉴창 구조작성및  애니메이션 추가

--- js/script.js
+++ js/script.js
@@ -1,2 +1,45 @@
 console.clear();
 gsap.registerPlugin(ScrollTrigger);
+
+const headerMenuWrap = document.querySelector(".menuwrap");
+const headerWrap = document.querySelector(".header-wrap");
+const menuBtnBox = document.querySelector("header .menu-btn-box");
+const fullScreenMenu = document.querySelector(".fullscreen-menu");
+const screenMenuCloseBtn = document.querySelector(
+  ".fullscreen-menu .close-btn"
+);
+function headerMenuHovered() {
+  headerMenuWrap.addEventListener("mouseenter", function () {
+    addClass(headerWrap, "on");
+  });
+  headerMenuWrap.addEventListener("mouseleave", function () {
+    removeClass(headerWrap, "on");
+  });
+}
+
+function fullScreenMenuInit() {
+  menuBtnBox.addEventListener("click", function () {
+    const isActive = fullScreenMenu.classList.contains("active");
+    console.log(isActive);
+    if (!isActive) {
+      addClass(fullScreenMenu, "active");
+    }
+  });
+  screenMenuCloseBtn.addEventListener("click", function () {
+    const isActive = fullScreenMenu.classList.contains("active");
+    console.log(isActive);
+
+    if (isActive) {
+      removeClass(fullScreenMenu, "active");
+    }
+  });
+}
+function addClass(el, className) {
+  el.classList.add(className);
+}
+function removeClass(el, className) {
+  el.classList.remove(className);
+}
+
+headerMenuHovered();
+fullScreenMenuInit();
